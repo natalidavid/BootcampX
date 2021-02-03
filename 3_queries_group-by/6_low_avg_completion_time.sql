448b183204b72bbbe1b12ddf4b7f0df63904af7c
Natalie Davidovic <natalija.david@hotmail.com>
2021-02-03 03:27:14 +0000

    Add teachers & asst requests

--- 3_queries_group-by/6_low_avg_completion_time.sql
+++ 3_queries_group-by/6_low_avg_completion_time.sql
@@ -5,7 +5,7 @@ FROM assignment_submissions
 JOIN students ON students.id = student_id
 JOIN assignments ON assignments.id = assignment_id
 WHERE students.end_date IS NULL
-GROUP BY students.name
+GROUP BY student
 HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
 ORDER BY average_assignment_duration;
 
